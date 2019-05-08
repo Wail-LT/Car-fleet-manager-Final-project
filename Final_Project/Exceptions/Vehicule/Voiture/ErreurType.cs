414558e69062d7d64abf8dfeb3140935f5be4f87
Wells <latif.wail@hotmail.com>
2019-05-08 14:31:02 +0200

    validate

---
+++ Final_Project/Exceptions/Vehicule/Voiture/ErreurType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_Project.Exceptions.Voiture
+{
+    public class ErreurType : Exception
+    {
+        public ErreurType() : base(
+            "ERREUR : le type saisie ne fait pas partie de la liste")
+        {
+        }
+    }
+}
